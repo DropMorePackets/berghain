437a13b7bffd9378e15dbbe3eaff5271e1b85f2f
Niklas Mollenhauer <nikeee@outlook.com>
2025-05-29 14:13:36 +0200

    Upgrade vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -13,11 +13,4 @@ export default defineConfig({
         cssCodeSplit: false,
         assetsInlineLimit: 100000000,
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                api: 'modern-compiler'
-            }
-        }
-    }
 });
