1dd0f6e17e2250274645e772d5a7f34068725406
Niklas Mollenhauer <nikeee@outlook.com>
2025-06-15 18:12:06 +0200

    feat(web): Use env files for different entrypoints

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,16 +1,29 @@
-import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import { dirname, resolve } from "node:path";
+import { defineConfig, loadEnv } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import { createHtmlPlugin } from "vite-plugin-html";
 
-export default defineConfig({
-    plugins: [
-        viteSingleFile(),
-        createHtmlPlugin({
-            minify: true,
-        }),
-    ],
-    build: {
-        cssCodeSplit: false,
-        assetsInlineLimit: 100000000,
-    },
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            viteSingleFile(),
+            createHtmlPlugin({
+                minify: true,
+            }),
+        ],
+        build: {
+            cssCodeSplit: false,
+            assetsInlineLimit: 100000000,
+            rollupOptions: {
+                input: {
+                    main: resolve(__dirname, env.VITE_ENTRYPOINT ?? "index.html"),
+                },
+            },
+        },
+    };
 });
