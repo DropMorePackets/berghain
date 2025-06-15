b12fb10981f4ad7f2d272e0e527c525eebb8a8a3
Fionera <fionera@fionera.de>
2025-06-16 01:15:31 +0200

    ci: add js ci run

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     css: {
         preprocessorOptions: {
             scss: {
-                api: 'modern-compiler',
+                api: "modern-compiler",
             },
         },
     },
